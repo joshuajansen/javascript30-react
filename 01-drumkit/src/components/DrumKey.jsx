b902bba79bae49f4164e8cdac5b8a85c3404cae2
Joshua Jansen <joshuajansen88@gmail.com>
2018-10-15 17:47:18 +0200

    Refactor the way references are set for DrumKey component

--- 01-drumkit/src/components/DrumKey.jsx
+++ 01-drumkit/src/components/DrumKey.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import "./DrumKey.css"
 
 class DrumKey extends React.Component {
+  audioElement = React.createRef()
+  drumKeyElement = React.createRef()
+
   componentDidUpdate() {
     if(this.props.isPlaying){
-      this.audioElement.currentTime = 0;
-      this.audioElement.play();
+      this.audioElement.current.currentTime = 0;
+      this.audioElement.current.play();
     }
 
-    this.drumKeyElement.addEventListener("transitionend", () => {
-      this.drumKeyElement.classList.remove("isPlaying")
+    this.drumKeyElement.current.addEventListener("transitionend", () => {
+      this.drumKeyElement.current.classList.remove("isPlaying")
     })
   }
 
   render () {
     return (
-      <div ref={ e => this.drumKeyElement = e } className={this.props.isPlaying ? "drumKey isPlaying" : "drumKey"}>
+      <div ref={this.drumKeyElement} className={this.props.isPlaying ? "drumKey isPlaying" : "drumKey"}>
         <kbd>{this.props.keyLabel}</kbd>
         <span className="effectName">{this.props.effectName}</span>
-        <audio ref={ e => this.audioElement = e } src={this.props.soundEffect}></audio>
+        <audio ref={this.audioElement} src={this.props.soundEffect}></audio>
       </div>
     )
   }
