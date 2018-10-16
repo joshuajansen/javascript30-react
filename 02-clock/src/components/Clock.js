6557b789fdf6db2380ac2001b95d393f7fb2d326
Joshua Jansen <joshuajansen88@gmail.com>
2018-10-16 09:04:06 +0200

    Refactor clock to be stateless

--- 02-clock/src/components/Clock.js
+++ 02-clock/src/components/Clock.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import './Clock.css';
 import Hand from "./Hand.jsx"
 
-class Clock extends Component {
-  render() {
-    return (
-      <div className="clock">
-        <div className="clock-face">
-          <Hand handType="hour" />
-          <Hand handType="minute" />
-          <Hand handType="second" />
-        </div>
+const Clock = () => {
+  return (
+    <div className="clock">
+      <div className="clock-face">
+        <Hand handType="hour" />
+        <Hand handType="minute" />
+        <Hand handType="second" />
       </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default Clock;
